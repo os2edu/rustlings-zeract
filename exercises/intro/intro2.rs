a0a6b952d6f85d79a26ca647fc619b40aad74590
zeract <69899726+zeract@users.noreply.github.com>
2022-11-03 12:22:17 +0000

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
 
-
 fn main() {
     println!("Hello {}!","World");
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x:u8) -> u8{
+    if x>40{
+        x
+    }else{
+        2*x
+    }
+ }
 
 // Don't modify this function!
 #[test]
