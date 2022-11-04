910a0ca882d83fb69c4ae8c1462bd3296253397e
zeract <69899726+zeract@users.noreply.github.com>
2022-11-04 12:08:26 +0000

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -35,6 +35,27 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        let mut t:bool = false;
+        if scores.contains_key(&team_1_name){
+            t = true;
+        }
+        let mut team = scores.entry(team_1_name.clone()).or_insert(Team{name:team_1_name.clone(),goals_scored:team_1_score,goals_conceded:team_2_score});
+        if t==true{
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }
+        
+        
+        let mut t1:bool=false;
+        if scores.contains_key(&team_2_name){
+            t1 = true;
+        }
+        let mut team1 = scores.entry(team_2_name.clone()).or_insert(Team{name:team_2_name.clone(),goals_scored:team_2_score,goals_conceded:team_1_score});
+        if t1==true{
+            team1.goals_scored += team_2_score;
+            team1.goals_conceded += team_1_score;
+        }
+        
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
